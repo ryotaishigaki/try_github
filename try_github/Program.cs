bbd7e491e05ecb3db85e7c19d33a6b9021144e5a
ryotaishigaki <ishigakiryota@occ.co.jp>
2021-05-11 11:26:04 +0900

    Main クラスの編集

--- try_github/Program.cs
+++ try_github/Program.cs
@@ -7,6 +7,7 @@ namespace try_github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
